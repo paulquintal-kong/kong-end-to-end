f9a193f0b6bbdacef95f7b383205bfefb4ca9645
Paul Quintal <paul.quintal@konghq.com>
2026-01-08 16:48:07 +1100

    Fix outputs.tf syntax - use proper output blocks

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,29 @@
-control_plane_id       = ""
-control_plane_name     = ""
-control_plane_endpoint = ""
-service_id            = ""
-service_name          = ""
-service_url           = ""
+output "control_plane_id" {
+  description = "ID of the Kong Konnect control plane"
+  value       = konnect_gateway_control_plane.fhir_control_plane.id
+}
+
+output "control_plane_name" {
+  description = "Name of the Kong Konnect control plane"
+  value       = konnect_gateway_control_plane.fhir_control_plane.name
+}
+
+output "control_plane_endpoint" {
+  description = "Endpoint URL of the Kong Konnect control plane"
+  value       = konnect_gateway_control_plane.fhir_control_plane.config.control_plane_endpoint
+}
+
+output "service_id" {
+  description = "ID of the Kong service"
+  value       = konnect_gateway_service.fhir_service.id
+}
+
+output "service_name" {
+  description = "Name of the Kong service"
+  value       = konnect_gateway_service.fhir_service.name
+}
+
+output "service_url" {
+  description = "Full URL of the Kong service"
+  value       = "https://${konnect_gateway_service.fhir_service.host}${konnect_gateway_service.fhir_service.path}"
+}
